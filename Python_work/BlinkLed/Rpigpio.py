d34b8dcdd8222e6be55853a4de5d1555db7068d9
JDieffy <jpd16759@uga.edu>
2024-08-27 18:03:20 -0400

    RPIGPIO Square wave file

---
+++ Python_work/BlinkLed/Rpigpio.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+from time import sleep
+GPIO.setwarnings(0)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.OUT,initial=GPIO.LOW)
+
+freq = 200 #Frequency in herts
+per = 1/freq #period of the square wave
+dutycycle = .8 #number 0-1 .4 for 40% duty cycle high
+h = per * dutycycle # Calculate the time spent high 
+l = per - h			# then set the remainder of the period for low
+
+while True:
+    GPIO.output(4, GPIO.HIGH) #command high
+    sleep(h)
+    GPIO.output(4,GPIO.LOW) #command low
+    sleep(l)
