0f1944ad38020771e56d5dc3da5532f47b616a5a
Nevan Mukherjee <nm67037@uga.edu>
2024-09-05 16:21:46 -0400

    initial work on keypad detection - Nevan

---
+++ Python_work/7SD/keypad_detection.py
@@ -0,0 +1,43 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+x1 = 1 #need to edit these values to the
+x2 = 2 #actual GPIO pins. These numbers are placeholders
+x3 = 3
+x4 = 4
+
+y1 = 5
+y2 = 6
+y3 = 7
+y4 = 8
+
+GPIO.setup(x1, GPIO.OUT) #all the x (rows) are considered outputs to the Pi
+GPIO.setup(x2, GPIO.OUT)
+GPIO.setup(x3, GPIO.OUT)
+GPIO.setup(x4, GPIO.OUT)
+
+GPIO.setup(y1, GPIO.IN) #all the y (columns) are considered inputs to the Pi
+GPIO.setup(y2, GPIO.IN)
+GPIO.setup(y3, GPIO.IN)
+GPIO.setup(y4, GPIO.IN)
+
+def readkeypad(rownum,char): #this function needs to be told what row number it's looking at and what the characters in that row are
+    GPIO.output(rownum, GPIO.HIGH) 
+
+    columns = [y1, y2, y3, y4]
+    outval = None
+
+    for i in range(len(columns)): #using iteration, check each column for logic HIGH
+        if GPIO.input(columns[i]) == 1:
+            outval = char[i] #column n is associated with character n in char array from function input
+            break
+
+    GPIO.output(rownum, GPIO.LOW) #resetting the row to go back to basic off state
+    if outval is not None:
+        print(f"Button pressed: {outval}")
+    
+    return outval
+
+#now, we need t
