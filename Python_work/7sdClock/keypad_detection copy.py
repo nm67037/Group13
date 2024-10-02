b12d2dc09b5a459e7bf4eab36f273faf71bbb3e2
JDieffy <jpd16759@uga.edu>
2024-10-02 16:44:16 -0400

    final code uncommented

--- Python_work/7sdClock/keypad_detection copy.py
+++ Python_work/7sdClock/keypad_detection copy.py
@@ -1,17 +1,15 @@
 import RPi.GPIO as GPIO
-import time
 import math
-from time import sleep
 from datetime import datetime
-from datetime import timedelta
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
-led = 6
-clk0 = 5
-clk1 = 7
-clk2 = 16
-clk3 = 12
+#setting up GPIO's of the raspberry pi
+led = 6 #GPIO to Error Led
+clk0 = 5 #GPIO to first display's dff's clock pin
+clk1 = 7 #GPIO to second display's dff's clock pin
+clk2 = 16#GPIO to third display's dff's clock pin
+clk3 = 12#GPIO to fourth display's dff's clock pin
 DP = 11  # D-flip-flop input: 4D, 4Q
 A = 25 # input: D6, output: Q6
 B = 8 # input: 5D, output: 5Q
@@ -20,18 +18,7 @@ D = 23 # input: 2D, output: 2Q
 E = 22 # input: 1D, output: 1Q
 F = 10 # input: 7D, output: 7Q
 G = 24 # input: 8D, output: 8Q
-
-SSD_Pins = [ A, B, C, D, E, F, G]
-clks = [clk0, clk1, clk2, clk3]
-OPP= SSD_Pins 
-OPP.append(clks)
-OPP.append(led)
-OPP.append(DP)
-
-for j in range(len(OPP)): #defining gpios to dff as output
-    print(OPP[j])
-    GPIO.setup(OPP[j], GPIO.OUT,initial=GPIO.LOW)
-
+#GPIOs of the keypad, the rows of x will be outputs, and columns of y will be inputs
 x1 = 2 
 x2 = 3 
 x3 = 4
@@ -41,6 +28,24 @@ y1 = 15
 y2 = 18
 y3 = 17
 y4 = 27
+
+rows = [x1, x2, x3, x4]
+columns = [y1, y2, y3, y4]
+SSD_Pins = [ A, B, C, D, E, F, G]
+clks = [clk0, clk1, clk2, clk3]
+
+OPP = SSD_Pins 
+OPP.append(clks)
+OPP.append(led)
+OPP.append(DP)
+OPP.append(rows)
+
+for j in range(len(OPP)): #defining gpios to dff as output
+    print(OPP[j])
+    GPIO.setup(OPP[j], GPIO.OUT,initial=GPIO.LOW)
+
+for i in range(len(columns)): #defining y columns as input
+    GPIO.setup(columns[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 dt = [0, 0, 0, 0, 0, 0, 0, 10]
 cA = [1, 1, 1, 0, 1, 1, 1, 11]
@@ -60,27 +65,15 @@ N0 = [1, 1, 1, 1, 1, 1, 0, 0]
 hsh = [0, 0, 0, 0, 0, 0, 0, 15]
 
 numbers = [N0, N1, N2, N3, N4, N5, N6, N7, N8, N9]
-rows = [x1, x2, x3, x4]
-columns = [y1, y2, y3, y4]
 row1_chars = [N1,N2,N3,cA]
 row2_chars = [N4,N5,N6,cB]
 row3_chars = [N7,N8,N9,cC]
 row4_chars = [dt, N0, hsh, cD]
 characters = [row1_chars, row2_chars, row3_chars, row4_chars]
 
-global b_press_count
-b_press_count = 0 #creating a global counting variable to keep track of how many times b is pressed. Initializing it at 0.
-
-for j in range(len(rows)): #defining x rows as output
-    GPIO.setup(rows[j], GPIO.OUT)
-
-for i in range(len(columns)): #defining y columns as input
-    GPIO.setup(columns[i], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-global cmode,ssdstate,now,display,dspsel,flstime,dstate,diff,flashd,mminute,mhour
+global cmode,ssdstate,now,display,dspsel,flstime,dstate,diff,flashd,mminute,mhour,mset,b_press_count
 cmode = 0
 ssdstate = 1
-#display = [N0,N0,N0,N0]
 dspel = 0
 flstime = datetime.now()
 dstate = True
@@ -88,8 +81,11 @@ diff = 0
 flashd = 0
 mminute = 0
 mhour = 0
+mset = 0
+b_press_count = 0 #creating a global counting variable to keep track of how many times b is pressed. Initializing it at 0.
+
 def readkeypad(): #this function needs to be told what row number it's looking at and what the characters in that row are
-    global now, ssdstate, b_press_count, diff, flashd
+    global now, ssdstate, b_press_count, diff, flashd, mset
     outval = None
     while outval == None:
         for j in range(len(rows)) and range(len(characters)):
@@ -98,7 +94,7 @@ def readkeypad(): #this function needs to be told what row number it's looking a
             for i in range(len(columns)): #using iteration, check each column for logic HIGH
                 if GPIO.input(columns[i]) == 1:
                     outval = characters[j][i] #column n is associated with character n in char array from function input
-                    sleep(0.35)
+                    delay(0.35)
                     break
 
             GPIO.output(rows[j], GPIO.LOW) #resetting the row to go back to basic off state
@@ -122,12 +118,15 @@ def readkeypad(): #this function needs to be told what row number it's looking a
                 print(diff)
                    
         if cmode == 1:
-            if (datetime.now().minute - now.minute) >= 1:    
-                if ssdstate == 1:
-                    automode()
+            if (datetime.now().minute - now.minute) != 0 and ssdstate == 1:    
+                automode()
         elif cmode == 2:
             if flashd < 4:
                 flash(flashd)
+            if mset == 1 and (datetime.now().minute - now.minute) != 0 and ssdstate == 1:
+                now = datetime.now()
+                manmode(True)
+
             pass
 
     return outval
@@ -146,15 +145,12 @@ def flash(dply):
 #            print("on")
             printssd(numbers[display[dply]],clks[dply])
         dstate = not(dstate)
-    else:
-        #print("cant flash")
-        pass
 
 def printssd(input,clock):
     for i in range(7):
         GPIO.output(SSD_Pins[i],input[i])
     GPIO.output(clock, GPIO.HIGH)
-    sleep(.001)
+    delay(.001)
     GPIO.output(clock, GPIO.LOW)
 
 def ssdoff():
@@ -165,9 +161,12 @@ def ssdoff():
     while readkeypad() != hsh:
         pass
     ssdstate = 1
-    if cmode == 1:
+    if cmode == 0:
+        startmode()
+    elif cmode == 1:
         automode()
-
+    elif cmode == 2:
+        manmode(False)
 
 def PMdot(bol):
     global display
@@ -182,10 +181,20 @@ def errLED(bol):
     else:
         GPIO.output(led,0)
 
+def delay(secs):
+    poll = datetime.now()#will fail on the border of x:59 and x+1:00 
+    starttime = poll.minute * 60 + poll.second + poll.microsecond/pow(10,6)
+    timecheck = starttime
+    while timecheck - starttime < secs:
+        poll = datetime.now()
+        timecheck = poll.minute * 60 + poll.second + poll.microsecond/pow(10,6)
+    #print(f"time elapsed:  {timecheck - starttime}")
+
 def startmode():
-    global cmode, display, flashd
+    global cmode, display, flashd, mset
     cmode = 0
     flashd = 0
+    mset = 0
     display = [0,0,0,0]
     errLED(0)
     print("startmode")
@@ -194,9 +203,11 @@ def startmode():
     modesel = readkeypad()
     if modesel == cA:
         print("auto mode")
+        cmode = 1
         automode()
     elif modesel == cB:
         print("manual mode")
+        cmode = 2
         manmode(False)
     elif modesel == hsh:
         print("display off/on")
@@ -204,9 +215,7 @@ def startmode():
 
 def automode():
     global cmode, now, display
-    cmode = 1
     now = datetime.now()
-    doffset = now.second
     hour = now.hour
     if now.hour > 12:
         hour -= 12
@@ -219,16 +228,13 @@ def automode():
     else:
         PMdot(False)
     while cmode == 1:
-        readkeypad()
-    #printssd(numbers[hour%10],clk1)
-    #printssd(numbers[math.floor(now.minute/10)],clk2)
-    #printssd(numbers[now.minute%10],clk3)
+        if readkeypad() == hsh:
+            ssdoff()
+    
 
-#df
-def manmode(set):
-    global cmode, display, flashd, mhour, mminute
-    cmode = 2
-    if not(set):
+def manmode(update):
+    global cmode, display, flashd, mhour, mminute, mset, now
+    if not(mset):
         inv = True
         print("first display")
         while inv:#set first seven segment display
@@ -266,14 +272,17 @@ def manmode(set):
         inv = True
         printssd(numbers[display[1]],clk1)
         mhour = display[0] * 10 + display[1]
-        if mhour > 12:
-            mhour -= 12
-            display = [math.floor(mhour/10), mhour%10, 0 , 0]
+        hour = mhour
+        if hour > 12:
+            hour -= 12
+            display = [math.floor(hour/10), hour%10, 0 , 0]
             printssd(numbers[display[0]],clk0)
             printssd(numbers[display[1]],clk1)
             PMdot(True)
         elif mhour == 12:
             PMdot(True)
+        elif hour == 0:
+            display = [1, 2, 0, 0]
         flashd = 2
         print("third display")
         while inv:
@@ -301,29 +310,33 @@ def manmode(set):
         printssd(numbers[display[3]],clk3)
         flashd = 4
         mminute = 10 * display[2] + display[3]
-    # if now.hour > 12:
-    #     hour -= 12
-    # #                   H1             H2               M1                  M2
-    # display = [math.floor(hour/10), hour%10, math.floor(now.minute/10) ,now.minute%10]
-    # for i in range(len(display)):
-    #     printssd(numbers[display[i]],clks[i])
-    # if now.hour >= 12:
-    #     PMdot(True)
-    # else:
-    #     PMdot(False)
-
-
-    
+        mset = 1
+        print("manual clock set")
+        now = datetime.now()
+    elif update:
+        mminute += 1
+        if mminute == 60:
+            mminute = 0
+            mhour += 1
+        if mhour == 24:
+            mhour = 0
+    hour = mhour
+    if hour > 12:
+        hour -= 12
+    elif hour == 0:
+        hour = 12
+    #                   H1             H2               M1                  M2
+    display = [math.floor(hour/10), hour%10, math.floor(mminute/10) ,mminute%10]
+    for i in range(len(display)):
+        printssd(numbers[display[i]],clks[i])
+    if mhour >= 12:
+        PMdot(True)
+    else:
+        PMdot(False)    
 
     while cmode == 2:
-        readkeypad()
-
-
-
-#now, we need to constantly poll the x outputs to see if any row was engaged. 
-#when the Pi detects an engaged row, we can call the readkeypad function
-#given the rownum argument and the character array inside that row.
-
+        if readkeypad() == hsh:
+            ssdoff()
 running = True
 while running:
     startmode()
